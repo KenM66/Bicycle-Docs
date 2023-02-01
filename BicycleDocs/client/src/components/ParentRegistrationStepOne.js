2f1c8c485c8991dbf7faca0a33840b8379fe8d05
KenM66 <milotakw@g.cofc.edu>
2023-01-31 23:44:30 -0600

    Parent Reg HTML step 1 completed.

--- BicycleDocs/client/src/components/ParentRegistrationStepOne.js
+++ BicycleDocs/client/src/components/ParentRegistrationStepOne.js
@@ -46,6 +46,29 @@ const ParentRegistrationStepOne=(props)=>{
         }
      })
 
+     return(
+        <div>
+            <h5>Contact Information</h5>
+
+            <div>
+                <label htmlFor="firstName">First Name</label>
+                <input name="firstName" type="text" id="firstName" value={formik.values.firstName} onChange= {formik.handleChange} onBlur={formik.handleBlur}/>
+                <div>{formik.errors.firstName && formik.touched.firstName?(<div><h5 style={{color: 'red'}}>{formik.errors.firstName}</h5></div>): null}</div><br/>
+
+                <label htmlFor="lastName">Last Name</label>
+                <input name="lastName" type="text" id= "lastName" value={formik.values.lastName} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+                <div>{formik.errors.lastName && formik.touched.lastName?(<div><h5 style={{color: 'red'}}>{formik.errors.lastName}</h5></div>): null}</div><br/>
+
+                <label htmlFor="phone">Phone #</label>
+                <MaskedInput mask={phoneNumberMask} name="phone" type="tel" value={formik.values.phone} id="phone" onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+                <div>{formik.errors.phone && formik.touched.phone?(<div><h5 style={{color: 'red'}}>{formik.errors.phone}</h5></div>): null}</div>
+
+            </div><br/><br/>
+            <button className='btn btn-info' type="submit" onClick={formik.handleSubmit}>Next</button>
+
+        </div>
+     )
+
 }
 
 export default ParentRegistrationStepOne;
