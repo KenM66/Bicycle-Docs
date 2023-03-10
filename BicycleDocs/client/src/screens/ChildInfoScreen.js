c7b20571d75bdd9696bf4db19a6d999e77f1934b
KenM66 <milotakw@g.cofc.edu>
2023-03-10 16:59:21 -0600

    Added part of Child Info screen

--- BicycleDocs/client/src/screens/ChildInfoScreen.js
+++ BicycleDocs/client/src/screens/ChildInfoScreen.js
@@ -9,6 +9,7 @@ import Loader from "../components/Loader";
 const ChildInfoScreen=()=>{
 
     const [image, setImage]= useState();
+    const [file, setFile]= useState();
 
     
     const imageSource= "http://localhost:5000/api/images/image-by-id/";
@@ -36,6 +37,21 @@ const ChildInfoScreen=()=>{
         }
     },[child])
 
+    const getChildAge=(dateOfBirth)=>{
+        const today= new Date();
+        const birthday= new Date(dateOfBirth);
+        let age= today.getFullYear()- birthday.getFullYear();
+        const m= today.getMonth()- birthday.getMonth();
+
+        if(m<0 ||(m===0 && today.getDate()< birthday.getDate())){
+            age--;
+        }
+        
+        return age;
+
+
+    }
+
   
    
 
@@ -50,16 +66,42 @@ const ChildInfoScreen=()=>{
     return (
         <div >
             <br/>
+          
             <div className="square border border-info" style={{width: '1500px', margin: '0 auto'}}><br/>
                 {loading &&(<Loader/>)}
                 {error &&(<Error/>)}
-                {success &&(<div>
-                <div style={{marginLeft: '-1200px'}}>
-                    {(image) &&(<img src= {imageSource+image} 
-                    style={{height: "200px" ,width: '230px'}}/>)} 
-                    
+               {success && ( 
+
+                <div>
+                    <div style={{display: 'flex',  flexDirection: 'row', justifyContent:'center'}}>
+                    <div align='left' style={{marginLeft: '-1000px'}}>
+                    {(image) &&(<img src= {imageSource+image}  
+                    style={{height: "200px" ,width: '230px'}}/>)} <br/><br/>
+                    <input
+                     filename={file}
+                     onChange={e => setFile(e.target.files[0])}
+                    type="file"
+                    accept='image/*'
+                    >
+                   </input>
+           
+                   
+                   
+                   </div> 
+                   <div align='left' style={{ display: 'flex', flexDirection: 'column'}}>
+                    <h4>{child.firstName} {child.lastName}</h4>
+                    <h4>Age: {getChildAge(child.dateOfBirth)}</h4>
                     </div>
-                </div>)}
+                   
+
+                </div> 
+                       
+                
+               
+              </div>  
+                
+                )}
+               
                 <br/>
             
           
